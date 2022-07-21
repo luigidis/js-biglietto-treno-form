f8a2a04b7299b2e7acfbe7ac403dafa78c00994d
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-21 16:07:11 +0200

    Human Code scritto

---
+++ js/app.js
@@ -0,0 +1,23 @@
+console.log('hello')
+
+// RECUPERO L'INPUT IN CUI L'UTENTE METTE QUANTI KM DOVRA' PERCORRERE E LO METTERO' DENTRO UNA VARIABILE
+const inputElement = document.querySelector('input[name="distance"]')
+console.log(inputElement.value)
+// RECUPERO LA FASCIA D'ETA' DELL'UTENTE E LO METTERO' IN UNA VARIABILE
+let inputUserAge = document.querySelector('user_age')
+console.log(inputUserAge)
+
+// CALCOLO IL PREZZO PIENO DEL BIGLIETTO
+
+// CALCOLO LO SCONTO SE E' MINORENNE * 0.20
+
+// CALCOLO LO SCONTO SE E' OVER 65 * 0.40
+
+// GENERO IL PREZZO FINALE
+
+// STAMPO IL RISULTATO NEL MIO FILE HTML
+
+
+
+
+
