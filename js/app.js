bb4156f16cfbf27bfb57219701562e9aa36dfdcb
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-21 17:21:27 +0200

    Milestone 1

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ console.log('hello')
 const priceKm = 0.21
 const discountTeen = 0.20
 const discountOver65 = 0.40
+
+//PRENDO IL MIO TAG HTML DOVE DEVO STAMPARE IL PREZZO
+const testoDaRestituire = document.getElementById('testo_da_restituire')
+console.dir(testoDaRestituire)
+
 // SE L'USERNAME CLICCA SUL TASTO GENERA
 const buttonElement = document.querySelector('input[type="submit"]')
 console.log(buttonElement)
@@ -21,19 +26,25 @@ buttonElement.addEventListener('click',function () {
     // CALCOLO IL PREZZO PIENO DEL BIGLIETTO E LO METTO IN UNA VARIABILE
     const fullPrice = parseFloat(inputElementDistance.value) * priceKm
     console.log(fullPrice)
-    // A QUESTO PUNTO INIZIO IL MIO ALGORITMO PER LO SCONTO SE NECESSARIO
+
+    
+
+    // A QUESTO PUNTO INIZIO IL MIO ALGORITMO PER LO SCONTO 
     if (inputUserAgeValue === 1) {
         const discount = fullPrice * discountTeen
         console.log(discount)
         const finalPrice = fullPrice - discount
         alert(finalPrice)
+        testoDaRestituire.innerHTML = "Il tuo biglietto ha un prezzo di: " + finalPrice.toFixed(2)
     } else if (inputUserAgeValue === 3) {
         const discount = fullPrice * discountOver65
         console.log(discount)
         const finalPrice = fullPrice - discount
         alert(finalPrice)
+        testoDaRestituire.innerHTML = "Il tuo biglietto ha un prezzo di: " + finalPrice.toFixed(2)
     } else if (inputUserAgeValue === 2) {
         alert(fullPrice)
+        testoDaRestituire.innerHTML = "Il tuo biglietto ha un prezzo di: " + fullPrice.toFixed(2)
     } else {
         alert('Inserisci età valida')
     }
