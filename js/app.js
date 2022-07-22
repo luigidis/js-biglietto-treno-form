f333166ef08ef92fd013ff3c6dcb72806aa5a34e
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-22 11:03:16 +0200

    Creating ticket to print (in progress)

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-console.log('hello')
+// console.log('hello')
 //VARIABILI COSTANTI
 const priceKm = 0.21
 const discountTeen = 0.20
@@ -6,49 +6,53 @@ const discountOver65 = 0.40
 
 //PRENDO IL MIO TAG HTML DOVE DEVO STAMPARE IL PREZZO
 const testoDaRestituire = document.getElementById('testo_da_restituire')
-console.dir(testoDaRestituire)
+// console.dir(testoDaRestituire)
 
 // SE L'USERNAME CLICCA SUL TASTO GENERA
 const buttonElement = document.querySelector('input[type="submit"]')
-console.log(buttonElement)
+// console.log(buttonElement)
 buttonElement.addEventListener('click',function () {
     // RECUPERO L'INPUT IN CUI L'UTENTE METTE QUANTI KM DOVRA' PERCORRERE E LO METTERO' DENTRO UNA VARIABILE
     const inputElementDistance = document.querySelector('input[name="distance"]')
-    console.log(inputElementDistance.value)
+    // console.log(inputElementDistance.value)
     // RECUPERO LA FASCIA D'ETA' DELL'UTENTE E LO METTERO' IN UNA VARIABILE
     let inputUserAge =  document.querySelector('#user_age') 
-    console.log(inputUserAge.value)
+    // console.log(inputUserAge.value)
     let inputUserAgeValue = parseFloat(inputUserAge.value)
-
-    // console.dir(inputUserAge)
-    // console.log(inputUserAge)
-    
     // CALCOLO IL PREZZO PIENO DEL BIGLIETTO E LO METTO IN UNA VARIABILE
     const fullPrice = parseFloat(inputElementDistance.value) * priceKm
-    console.log(fullPrice)
-
-    
-
+    // console.log(fullPrice)
     // A QUESTO PUNTO INIZIO IL MIO ALGORITMO PER LO SCONTO 
     if (inputUserAgeValue === 1) {
         const discount = fullPrice * discountTeen
-        console.log(discount)
+        // console.log(discount)
         const finalPrice = fullPrice - discount
-        alert(finalPrice)
+        // alert(finalPrice)
         testoDaRestituire.innerHTML = "Il tuo biglietto ha un prezzo di: " + finalPrice.toFixed(2) + "€"
     } else if (inputUserAgeValue === 3) {
         const discount = fullPrice * discountOver65
-        console.log(discount)
+        // console.log(discount)
         const finalPrice = fullPrice - discount
-        alert(finalPrice)
+        // alert(finalPrice)
         testoDaRestituire.innerHTML = "Il tuo biglietto ha un prezzo di: " + finalPrice.toFixed(2) + "€"
     } else if (inputUserAgeValue === 2) {
-        alert(fullPrice)
+        // alert(fullPrice)
         testoDaRestituire.innerHTML = "Il tuo biglietto ha un prezzo di: " + fullPrice.toFixed(2) + "€"
     } else {
         alert('Inserisci età valida')
     }
 })
+
+//Recuperare l'elemento del button Annulla
+const resetElement = document.querySelector('input[type="button"]')
+console.log(resetElement)
+// agganciare un event listener che ascolta il click su Annulla
+// Una volta cliccato dobbiamo resettare i valori del form
+    
+    
+
+    
+
         
         
 
