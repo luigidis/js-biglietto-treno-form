305b7bd64e386b9621b6095bf1338b3af11bbafe
LuigiDiS <luigi.distefano69@gmail.com>
2022-07-22 11:03:33 +0200

    Fixed button "Annulla"

--- js/app.js
+++ js/app.js
@@ -7,18 +7,21 @@ const discountOver65 = 0.40
 //PRENDO IL MIO TAG HTML DOVE DEVO STAMPARE IL PREZZO
 const testoDaRestituire = document.getElementById('testo_da_restituire')
 // console.dir(testoDaRestituire)
+// RECUPERO L'INPUT IN CUI L'UTENTE METTE QUANTI KM DOVRA' PERCORRERE E LO METTERO' DENTRO UNA VARIABILE
+const inputElementDistance = document.querySelector('input[name="distance"]')
+// console.log(inputElementDistance.value)
+// RECUPERO LA FASCIA D'ETA' DELL'UTENTE E LO METTERO' IN UNA VARIABILE
+let inputUserAge =  document.querySelector('#user_age') 
+// console.log(inputUserAge.value)
+let inputUserAgeValue = parseFloat(inputUserAge.value)
+// RECUPERO input del NOME E COGNOME utente e lo metto in una variabile
+const inputUserName = document.querySelector('input[name="username"]')
+console.log(inputUserName.value)
 
 // SE L'USERNAME CLICCA SUL TASTO GENERA
 const buttonElement = document.querySelector('input[type="submit"]')
 // console.log(buttonElement)
 buttonElement.addEventListener('click',function () {
-    // RECUPERO L'INPUT IN CUI L'UTENTE METTE QUANTI KM DOVRA' PERCORRERE E LO METTERO' DENTRO UNA VARIABILE
-    const inputElementDistance = document.querySelector('input[name="distance"]')
-    // console.log(inputElementDistance.value)
-    // RECUPERO LA FASCIA D'ETA' DELL'UTENTE E LO METTERO' IN UNA VARIABILE
-    let inputUserAge =  document.querySelector('#user_age') 
-    // console.log(inputUserAge.value)
-    let inputUserAgeValue = parseFloat(inputUserAge.value)
     // CALCOLO IL PREZZO PIENO DEL BIGLIETTO E LO METTO IN UNA VARIABILE
     const fullPrice = parseFloat(inputElementDistance.value) * priceKm
     // console.log(fullPrice)
@@ -42,12 +45,25 @@ buttonElement.addEventListener('click',function () {
         alert('Inserisci età valida')
     }
 })
-
+   
 //Recuperare l'elemento del button Annulla
 const resetElement = document.querySelector('input[type="button"]')
-console.log(resetElement)
+// console.log(resetElement)
 // agganciare un event listener che ascolta il click su Annulla
+resetElement.addEventListener('click', function(){
+    inputUserAge.value = '---'
+    inputElementDistance.value = '0'
+    inputUserName.value = ''
+
+})
 // Una volta cliccato dobbiamo resettare i valori del form
+
+
+
+
+
+
+
     
     
 
